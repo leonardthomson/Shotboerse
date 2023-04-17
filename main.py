6f02eb09ed8fe3a07fc72fdbba3051f43db61657
Dun <danielburkhardt@freenet.de>
2023-04-17 17:19:07 +0200

    Cosmetics

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 # imports
 ## general
+import glob
 import json
+from datetime import datetime
 from logging import raiseExceptions
 import pyqtgraph as pg
+import os
 
 import sys
 from random import randint
 import numpy as np
 import pyttsx3
 ## PySide
-from PySide6.QtWidgets import QDialogButtonBox, QPushButton, QApplication, QMainWindow, QDialog, QInputDialog, QVBoxLayout, QGroupBox, QGridLayout, QCheckBox, QFileDialog
+from PySide6.QtWidgets import QDialogButtonBox, QPushButton, QApplication, QMainWindow, QDialog, QInputDialog, \
+    QVBoxLayout, QGroupBox, QGridLayout, QCheckBox, QFileDialog, QLabel, QSizePolicy
 from PySide6.QtCore import QSize, Qt, QCoreApplication
+from PySide6.QtGui import QFont, QShortcut, QKeySequence
 from PySide6 import *
 ## own
 from myWidget import Ui_MainWindow
 from initWindow import Ui_Dialog
 
-# TODO: Schreibe aktuelle Preise ans Ende des Graphs
-# TODO: GIB DIE KOSTEN-Berechnung aus
-# TODO: Öffne die Nachfrage schön! InputWidget anpassen!
-# TODO: Evtl Fehlerbehandlung
+import random_events
+
+# TODO DB: Maybe count, how often every shot was bought
 
 # Constant
+# Mexi, Gin, Tequila, Vodka
+
 ALL_SHOTS = ["Mexikaner", "Gimlet", "Blueshot",
              "Fishshot", "Tequila", "Vodka Brause",
              "Berentzen", "Joster", "Pfeffi"]
@@ -31,24 +37,47 @@ ALL_COLORS = [(189, 29, 23), (68, 250, 177), (60, 182, 204),
 ALL_DIC = dict(zip(ALL_SHOTS, ALL_COLORS))
 
 # Current Shot selection
-shot_selection = ["Mexikaner", "Gimlet", "Fishshot", "Tequila", "Vodka Brause"] #ALL_SHOTS
+shot_selection = ["Mexikaner", "Gimlet", "Tequila", "Vodka Brause"] #ALL_SHOTS
 assert all(names in ALL_SHOTS for names in shot_selection),\
     f"Unrecognized Shot! Allowed shots are:\n{ALL_SHOTS}"
-shot_dic = {names:pg.mkPen(width=4, color=ALL_DIC[names]) for names in shot_selection}
+shot_dic = {names: pg.mkPen(width=4, color=ALL_DIC[names]) for names in shot_selection}
 
 # Inits
+
+# Time, after which a shot automatically updates (in seconds)
+update_time = 1
+# The noise we add in each time step
+step_noise = .7
+
+# Time, after which a stock event occurs (in seconds)
+event_time = 20
+# Events occure every event_time +- event_time_variance(in seconds)
+event_time_variance = 2
+
+# Time, after which we save the y_data (in minutes)
+logging_minutes = 5
+
+# Buying a shot increases it's price by this size
 val_price_increase = 4
+
+
+# Price, from which the shots start
 avg = 100
-min_price = 30
-is_cheap_value = 60
+
+# Minimal price for shots. If it goes lower, we just choose min_price
+min_price = 50
+
+# From this point on we consider the shot to be cheap
+is_cheap_value = 50
 is_already_cheap = [0 for i in range(len(shot_dic))]
+
 # TODO DB: UPDATE NUMBER OF X-VALUES
-n_x_values = 30
+n_x_values = 1000
 #engine = pyttsx3.init()
+
 
 ### Speak
 class _TTS:
-
     engine = None
     rate = None
     def __init__(self):
@@ -65,8 +94,7 @@ class MyDialog(Ui_Dialog):
         super().setupUi(Dialog)
         self.log_file = None
         self.toolButtonLogFile.clicked.connect(self.set_log_file)
-    
-    
+
     def extract_checks(self):
         ls = [self.mex.isChecked(), self.gim.isChecked(), self.blu.isChecked(),
                 self.fis.isChecked(), self.teq.isChecked(), self.vod.isChecked(),
@@ -95,19 +123,65 @@ class MyMainWindow(QMainWindow):
         self.shot_names = []
         self.shot_dic = dict()
         self.count_dic = dict()
+        self.shots_bought_string = " "
         self.ui = Ui_MainWindow()
         self.init_dialog()
         self.ui.setupUi(self)
         self.show()
         self.set_up_graph()
-        self.shots_bought_string = " "
+
+
+        # Flag, whether the shot was already announced as being cheap
         self.is_already_cheap = [0 for _ in range(self.n_shots)]
 
-        # TODO: INSERT POTENTIAL TIMER HERE
-        #self.timer = pg.QtCore.QTimer()
-        #self.timer.timeout.connect(self.use_le_input())
+        # TIMERS
+        self.timer_seconds = pg.QtCore.QTimer()
+        self.timer_seconds.setInterval(update_time*1000)
+        self.timer_seconds.timeout.connect(self.noised_time_step)
+        self.timer_seconds.start()
+
+        self.timer_events = pg.QtCore.QTimer()
+        self.timer_events.setInterval(event_time*1000)
+        self.timer_events.timeout.connect(self.stock_event)
+        self.timer_events.start()
+
+        # Time until we save the y_data
+        self.timer_logging = pg.QtCore.QTimer()
+        self.timer_logging.setInterval(logging_minutes * 60 * 1000)
+        self.timer_logging.timeout.connect(self.save_y_data_as_file)
+        self.timer_logging.start()
+
+        self.all_events = random_events.get_events()
+        self.event_idx = 0
+        self.n_events = len(self.all_events)
+
+        self.output_window = QDialog()
+        self.output_window.setWindowFlags(Qt.CustomizeWindowHint | Qt.FramelessWindowHint)
+        self.output_window.setStyleSheet("background-color: black")
+        self.output_window.setStyleSheet("border: 2px solid red;")
+        self.output_label = QLabel("Output Window", self.output_window)
+        self.output_label.setWordWrap(True)
+        self.output_label.setAlignment(Qt.AlignCenter)
+        self.output_label.setFont(QFont("Arial", 50))
+        self.output_label.setGeometry(300,200,300,300)
+        self.output_label.setStyleSheet("color: red")
+
+        self.output_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        shortcut = QShortcut(QKeySequence(Qt.Key_P), self.output_window)
+        shortcut.activated.connect(self.output_window.close)
+
+        #self.output_label.setSizePolicy(PySide6.QtWidgets.)
+        #self.output_window.set_title("EVENT OCCURED!")
 
         self.ui.lineEdit.returnPressed.connect(self.use_le_input)
+
+    def closeEvent(self, *args, **kwargs):
+        super(MyMainWindow, self).closeEvent(*args, **kwargs)
+        # Here we can simply save the y_data!
+        print("We saved the data!")
+        self.save_y_data_as_file()
+
 
     def init_dialog(self):
         d = QDialog()
@@ -129,27 +203,43 @@ class MyMainWindow(QMainWindow):
             self.count_dic = {x:0 for x in self.shot_names}
             self.count_dic["Umsatz"] = 0
 
+    def save_y_data_as_file(self):
+        # Use numpy to save the y_data and give a time_stamp
+        time_stamp = datetime.now().strftime("%Y-%m-%d_%H")
+        np.save(".\\time_stamps\y_data_"+str(time_stamp), self.y_data)
+
+    def load_y_data(self, path):
+        if path:
+            # We load the newest y_data file in our current folder
+            # RIGHT NOW WE ONLY LOAD THE NEWEST ONE
+            files = glob.glob(r".\time_stamps\y_data_*.npy")
+            if len(files) == 0:
+                print("No y_data file found!")
+                return 0
+            files.sort(key=os.path.getmtime)
+            self.y_data = np.load(files[-1])
+            #self.update_price()
+            #self.update_shots_bought()
 
     def set_up_graph(self):
         """
         Initializes the Graph with constanst entries.
         """
         self.ui.graphicsView.addLegend()
-
-        # styles = {'color': 'r', 'font-size': '50px'}
-        #self.ui.graphicsView.setLabel('left', "Preis in Cent", **styles)
-        # self.setWindowTitle("STONKS!")
-
-        # INITIALIZE THE GRAPHS with 100 points
+        self.ui.graphicsView.setLabel('left', "Price", units='.01€')
+        # INITIALIZE THE GRAPHS with n_x_values points
         self.x = np.arange(n_x_values)
-        self.y = np.random.rand(n_x_values) * 2 * avg
-        self.y_data = np.full(shape=(self.n_shots, n_x_values), fill_value=100)
+        if not self.log_bool:
+            self.y_data = random_events.valid_random_walks(self.n_shots, n_x_values, avg, 2, epsilon=15)
+        else:
+            self.load_y_data(self.log_file_path)
+
         self.data_lines = [
             self.ui.graphicsView.plot(
                 self.x, y_data,pen=self.shot_dic[name],
-                name=name, symbol='o', symbolSize=10)
+                name=name, symbol='o', linewidth=.5, symbolSize=2)
             for y_data, name in zip(self.y_data, self.shot_dic)]
-        self.price = np.full(shape=self.n_shots, fill_value=100)
+        self.price = self.y_data[:,-1]#np.full(shape=self.n_shots, fill_value=100)
         self.shots_bought = np.zeros(shape=self.n_shots)
         self.set_prices()
         self.print_price()
@@ -163,14 +253,23 @@ class MyMainWindow(QMainWindow):
         self.shots_bought_string = self.ui.lineEdit.text()
         self.shots_bought_string = self.shots_bought_string.replace(" ", "")
         self.ui.lineEdit.clear()
-        if self.shots_bought_string == "random":
+
+        # HARDCODED SPECIAL CASES
+        if self.shots_bought_string == "stoptimer":
+            self.timer_seconds.stop()
+        elif self.shots_bought_string == "starttimer":
+            self.timer_seconds.start()
+        elif self.shots_bought_string == "stopevents":
+            self.timer_events.stop()
+        elif self.shots_bought_string == "startevents":
+            self.timer_events.start()
+        elif self.shots_bought_string == "random":
             self.random_walk()
         elif self.shots_bought_string == "longrandom":
             self.random_walk(nWalks=20)
         elif self.shots_bought_string == "reset":
             self.reset()
         elif self.shots_bought_string == "clear":
-
             self.y_data = np.full(shape=(self.n_shots, n_x_values), fill_value=100)
             self.x = np.arange(n_x_values)
             self.reset()
@@ -213,7 +312,7 @@ class MyMainWindow(QMainWindow):
         Calculate prices.
         """
         self.ui.priceList.clear()
-        self.ui.priceList.addItems([shot_name+": "+str(round(price)/100) +"€"\
+        self.ui.priceList.addItems([f"{shot_name}: {(price/100):.2f}€"
             for shot_name, price in zip(self.shot_names, self.price)])
         for idx in range(self.n_shots):
             if self.price[idx] < is_cheap_value and not is_already_cheap[idx]:
@@ -228,10 +327,10 @@ class MyMainWindow(QMainWindow):
         """
         self.x += 1
         self.y_data[:,:-1] = self.y_data[:,1:]
-        self.y_data[:,-1] = self.price  # Add a new random value.
-        [data_line.setData(self.x, self.y_data[idx][:])\
-            for idx, data_line in enumerate(self.data_lines)] # Update the data.
+        self.y_data[:,-1] = self.price  # Add the new price value
 
+        for idx, data_line in enumerate(self.data_lines):# Update the data.
+            data_line.setData(self.x, self.y_data[idx][:])
     def update_shots_bought(self):
         """
         Updates Attribute.
@@ -244,8 +343,49 @@ class MyMainWindow(QMainWindow):
         """
         val1 = sum(self.shots_bought)/(self.n_shots-1)
         val2 = (1+1/(self.n_shots-1))
-        self.price = [max(last_price + val_price_increase*(val2 * n_shots_bought - val1), min_price)\
-            for last_price, n_shots_bought in zip(self.y_data[:,-1], self.shots_bought)]
+
+        self.price = [max(last_price + val_price_increase*(val2 * n_shots_bought - val1), min_price) \
+            for last_price, n_shots_bought in zip(self.y_data[:, -1], self.shots_bought)]
+
+    def noised_time_step(self, noise=step_noise):
+        """
+        Simulates one time step.
+        """
+        self.price = [max(self.price[idx] + noise*(np.random.rand()-0.5), min_price) for idx in range(self.n_shots)]
+        self.update_plot_data()
+        self.set_prices()
+        self.print_price()
+
+    def stock_event(self):
+        """
+        Triggers the next event.
+
+        """
+        # Choose event
+        current_evt =self.all_events[self.event_idx]
+        event_text = current_evt[0]
+        price_change = current_evt[1]
+
+        self.output_label.setText(event_text)
+        self.output_window.setStyleSheet("background-color: black")
+        self.output_window.setStyleSheet("border: 2px solid red;")
+        self.output_label.adjustSize()
+        self.output_window.show()
+
+        # Update event_idx
+        self.event_idx += 1
+        if self.event_idx >= self.n_events:
+            # Return a new list of events
+            self.all_events = random_events.get_events()
+            self.event_idx = 0
+
+        self.price = [max(self.price[idx] + price_change[idx], min_price) for idx in range(self.n_shots)]
+        self.update_plot_data()
+        self.set_prices()
+        self.print_price()
+
+        # Set a new event-timing
+        self.timer_events.setInterval((event_time + np.random.randint(-event_time_variance, event_time_variance))* 1000)
 
 
     def reset(self):
@@ -258,24 +398,16 @@ class MyMainWindow(QMainWindow):
         self.set_prices()
         self.ui.pay.setText("VON NEUEM!")
 
-    def random_walk(self, nWalks=5, n_shots=5):
+    def random_walk(self, nWalks=5):
         """
         Simulates 5 random orders.
         """
         for _ in range(nWalks):
-            self.shots_bought = [randint(0, n_shots) for _ in range(self.n_shots)]
+            self.shots_bought = [randint(0, self.n_shots) for _ in range(self.n_shots)]
             self.update_price()
             self.update_plot_data()
         self.set_prices()
-        self.ui.pay.setText("Kleiner Randomwalk gefällig?")
-
-    def repeat_current_data(self):
-        """
-        constant graph?
-        """
-        self.shots_bought = [0 for _ in range(self.n_shots)]
-        self.update_price()
-        self.update_plot_data()
+        #self.ui.pay.setText("Kleiner Randomwalk gefällig?")
 
     def praise_shots(self, idx):
         """
@@ -285,28 +417,21 @@ class MyMainWindow(QMainWindow):
         price = self.price[idx]
         n_praises = 3
         random_idx = randint(0, n_praises-1)
-        shout_out = ""
-        if random_idx == 0:
-            shout_out = (
-                f"{shot_name} ist billig! Kauft {shot_name}"
-                f"{shot_name} nur {str(round(price))} Cent!"
-            )
-        elif random_idx == 1:
-            shout_out = (
-                f"Kauft {shot_name}!"
-                f"Er ist billig und willig!"
-            )
-        elif random_idx == 2:
-            shout_out = (
-                f"Der {shot_name}-Markt bricht zusammen!"
-                f"Kauft {shot_name}"
-            )
+
+        shoutouts = [(f"{shot_name} ist billig! Kauft {shot_name}", f"{shot_name} nur {str(round(price))} Cent!"),
+                     (f"Kauft {shot_name}!", f"Er ist billig und willig!"),
+                     (f"Der {shot_name}-Markt bricht zusammen!", f"Kauft {shot_name}!")]
+        shout_out = shoutouts[random_idx]
         engine = _TTS()
         engine.start(shout_out)
         del(engine)
 
+
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyMainWindow()
+    window.setWindowFlags(Qt.CustomizeWindowHint | Qt.FramelessWindowHint)
     window.showMaximized()
     sys.exit(app.exec())
--- random_events.py
+++ random_events.py
@@ -1,39 +1,40 @@
 import random
+import numpy as np
 
 # Die Reihenfolge der Shots lautet:
 # Tequila, Vodka, Mexikaner, Gimlet
 
 event_list = [
-    ["Deflation: Alles wird bspw. 10ct billiger.", (-.1, -.1, -.1, -.1)],
-    ["Inflation: Alles wird bspw. 10ct teurer.", (.1,.1,.1,.1)],
+    ["Deflation! Alles wird billiger!", (-10, -10, -10, -10)],
+    ["Inflation: Alles wird teurer!", (10, 10, 10, 10)],
 
-    ["Die Gin-Lobby hat eine Tarifverhandlung gewonnen! Gin wird billiger!", (0,0,0,-.15)],
-    ["Corona-Ausgleich für Studierende! Mexi wird billiger!", (0,0,-.15,0)],
-    ["Soergel wird Bundeskanzler! Er subventioniert Gin!", (0,0,0,-.15)],
-    ["Man findet in Junkers Büro große Mengen Vodka-Brause, der Markt wird geflutet!", (0,-.15,0,0)],
-    ["Die Fachschaft empfängt eine großzügige Limetten-Spende. Gin wird billiger!", (0,0,0,-.15)],
-    ["Wegen des Klimawandels wachsen jetzt Agaven am Kaiserstuhl! Tequila wird billiger!", (-.15, 0,0,0)],
-    ["Genmanipulierte Tomaten bringen den Markt ins Wanken! Mexikaner wird billiger!", (0,0, -.15,0)],
+    ["Die Gin-Lobby hat eine Tarifverhandlung gewonnen!\n Gin wird billiger!", (0,-15,0,0)],
+    ["Corona-Ausgleich für Studierende!\n Mexi wird billiger!", (-15,0,0,0)],
+    ["Soergel wird Bundeskanzler!\n Er subventioniert Gin!", (0,-15,0,0)],
+    ["Man findet in Junkers Büro große Mengen Vodka-Brause.\n Der Markt wird geflutet!", (0,0,0,-15)],
+    ["Die Fachschaft empfängt eine großzügige Limetten-Spende.\n Gin wird billiger!", (0,-15,0,0)],
+    ["Wegen des Klimawandels wachsen jetzt Agaven am Kaiserstuhl!\n Tequila wird billiger!", ( 0,0,-15,0)],
+    ["Genmanipulierte Tomaten bringen den Markt ins Wanken!\n Mexikaner wird billiger!", (-15,0,0, 0)],
 
-    ["Der Alkohol-Konsum der Assistent:innen wirkt sich beträchtlich auf den Markt aus. Mexikaner wird teurer!",
-     (0, 0, .15, 0)],
-    ["Die Erstis sind frustriert über die Ana-Klausur. Vodka wird teurer!", (0, .15, 0, 0)],
-    ["Mexikanische Mauer wird gebaut! Tequila und Mexi werden teurer!", (.15, 0, .15, 0)],
-    ["Huber kauft beträchtliche Mengen des Weltmarkts an Gin auf. Gin wird rar!", (0, 0, 0, .15)],
-    ["Putin fuckt ab. Vodka wird teurer!", (0, .15, 0, 0)],
-    ["Ein kaputtes Schiff hat den Limetten Weltmarkt ins Wanken gebracht! Gin wird teurer!", (0,0,0,.15)],
+    ["Der Alkohol-Konsum der Assistent:innen wirkt sich beträchtlich auf den Markt aus.\n Mexikaner wird teurer!",
+     (15,0, 0, 0)],
+    ["Die Erstis sind frustriert über die Ana-Klausur.\n Vodka wird teurer!", (0, 0, 0, 15)],
+    ["Die Mexikanische Mauer wird gebaut!\n Tequila und Mexi werden teurer!", (15, 0, 15, 0)],
+    ["Huber kauft beträchtliche Mengen des Weltmarkts an Gin auf.\n Gin wird rar!", (0, 15, 0, 0)],
+    ["Putin fuckt ab.\n Vodka wird teurer!", (0,  0, 0,15)],
+    ["Ein kaputtes Schiff hat den Limetten Weltmarkt ins Wanken gebracht!\n Gin wird teurer!", (0,15,0,0)],
 
-    ["Sozialverbände protestieren gegen die hohen Lebenserhaltungskosten! Es passiert nichts!", (0,0,0,0)],
-    ["Der Bundeskanzler ist in einen Finanzskandal verwickelt! Es passiert nichts!", (0,0,0,0)],
-    ["Der Studiengang wird reakkreditiert. Es passiert nichts!", (0,0,0,0)],
-    ["Die Vorlesungen werden evaluiert. Es passiert nichts!", (0,0,0,0)],
-    ["Der StuRa veröffentlich eine Pressemitteilung gegen Diskriminierung. Es passiert nichts!", (0,0,0,0)],
-    ["Die Fachschaftssitzung geht mal wieder 3 Stunden. Es passiert nichts!", (0,0,0,0)],
-    ["Du kriegst eine nicht deklarierte Parteispende. Eine Gin-Option!", (0,0,0,0)],
-    ["Der mexikanische Präsident lädt Dich auf seine Jacht ein. Eine Mexikaner-Option!", (0,0,0,0)],
-    ["Beim Golfen lernst Du Gerhard Schröder kennen. Eine Vodka-Option!", (0,0,0,0)],
-    ["Du erhältst brisante Nachrichten über den mexikanischen Agrar-Minister. Um Dich zum Schweigen zu bringen, erhältst Du eine Tequila-Option!", (0,0,0,0)],
-    ["Du findest auf dem Dachboden Deinen alten Bitcoin-Usb-Stick. Du erhältst 2 Optionen Deiner Wahl!", (0,0,0,0)],
+    ["Sozialverbände protestieren gegen die hohen Lebenserhaltungskosten!\n Es passiert nichts!", (0,0,0,0)],
+    ["Der Bundeskanzler ist in einen Finanzskandal verwickelt!\n Es passiert nichts!", (0,0,0,0)],
+    ["Der Studiengang wird reakkreditiert.\n Es passiert nichts!", (0,0,0,0)],
+    ["Die Vorlesungen werden evaluiert.\n Es passiert nichts!", (0,0,0,0)],
+    ["Der StuRa veröffentlich eine Pressemitteilung gegen Diskriminierung.\n Es passiert nichts!", (0,0,0,0)],
+    ["Die Fachschaftssitzung geht mal wieder 3 Stunden.\n Es passiert nichts!", (0,0,0,0)],
+    ["Du kriegst eine nicht deklarierte Parteispende.\n Eine Gin-Option!", (0,0,0,0)],
+    ["Der mexikanische Präsident lädt Dich auf seine Jacht ein.\n Eine Mexikaner-Option!", (0,0,0,0)],
+    ["Beim Golfen lernst Du Gerhard Schröder kennen.\n Eine Vodka-Option!", (0,0,0,0)],
+    ["Du erhältst brisante Nachrichten über den mexikanischen Agrar-Minister.\n Um Dich zum Schweigen zu bringen, erhältst Du eine Tequila-Option!", (0,0,0,0)],
+    ["Du findest auf dem Dachboden Deinen alten Bitcoin-Usb-Stick.\n Du erhältst 2 Optionen Deiner Wahl!", (0,0,0,0)],
 ]
 
 def print_events():
@@ -55,9 +56,45 @@ def one_event(idx=None):
         idx = random.randint(0, len(event_list))
     return event_list[idx]
 
-def all_events():
-    return event_list
+def get_events():
+    """Returns a randomized list of events."""
+
+    event_permutation = list(np.random.permutation(len(event_list)))
+    randomized_event_list = [event_list[idx] for idx in event_permutation]
+    return randomized_event_list
+
+def do_a_random_walk(n_steps, start_value=100, step_size = 1):
+    """Returns a list of n_steps random values between -1 and 1."""
+    # Default start_value has to be set here
+    steps = np.zeros(n_steps)
+    steps[0] = start_value
+    for i in range(0, n_steps-1):
+        steps[i+1] = random.randint(-1, 1) * step_size + steps[i]
+    return steps
+
+def do_multiple_random_walks(n_walks, n_steps, start_value=100, step_size = 1):
+    """Returns a list of n_walks random walks with n_steps each."""
+    all_walks = np.zeros((n_walks, n_steps))
+    for i in range(n_walks):
+        all_walks[i, :] = do_a_random_walk(n_steps, start_value, step_size)
+    return all_walks
+
+def valid_random_walks(n_walks, n_steps, start_value=100, step_size=1, epsilon=15):
+    all_walks = do_multiple_random_walks(n_walks=n_walks, n_steps=n_steps, start_value=start_value, step_size=step_size)
+    while np.abs(error :=(sum(all_walks[:,-1]-start_value))) > epsilon:
+        #error = sum(np.abs(all_walks[:,-1]-start_value))
+        all_walks = do_multiple_random_walks(n_walks=n_walks, n_steps=n_steps, start_value=start_value, step_size=step_size)
+    print(f"We start with an starting error of {error} cents!")
+    return all_walks
+def smoothen_end_of_random_walks(all_walks, n_steps, step_size=100, avg=100):
+    for i in range(1, n_steps-1):
+        all_walks[:, -n_steps + i] = avg + (all_walks[:, -n_steps + i - 1] - avg)/n_steps*(n_steps -i) + np.random.random(all_walks.shape[0]) * step_size
+    return all_walks
+
 
 if __name__ == "__main__":
     print_events()
-    print(f"Debug_sums: {debug_event_list()}")
+    val_list = debug_event_list()
+    print(f"Debug_sums: {val_list}")
+    print(f"So the Increase through events is {sum(val_list)}")
+
